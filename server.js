6b88ea79f235f9e132b767c45765d812ed10349a
Flo <x.client.ich@gmail.com>
2017-09-05 19:19:03 +0200

    Fixed Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -7,11 +7,25 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({ origin: 'https://dsgnhb.de' }));
+//app.use(cors({ origin: 'https://dsgnhb.de' }));
 const port = process.env.PORT || 8080;
 
 const router = require('./routes.js');
 
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://dsgnhb.de');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+  // Pass to next layer of middleware
+  next();
+});
 
 app.get('/', function (req, res) {
   res.send('Meddl!');
