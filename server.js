d91e68995ac0d6505180c68f635fec54fcc32df4
Flo <x.client.ich@gmail.com>
2017-09-06 15:04:51 +0200

    Public root for Levels

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require('body-parser');
 const config = require("./config.json");
 const design = require("./functions/topdesign.js")
+const level = require("./functions/levels.js")
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,6 +19,7 @@ app.get('/', function (req, res) {
   res.send('Meddl!');
 })
 app.get('/posts/month',design.findAllMonth); // Get all Posts sorted by Month
+app.get('/levels',level.findAll);
 
 // Getting API Tokens
 const tokens = config.apiKeys
