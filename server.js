770ac32e507f606ca131d3df9966ccdec6f0877e
Lukas <lukvonstrom@web.de>
2017-09-21 19:01:02 +0200

    Code Cleanup according to remove unused parameters

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 const express = require('express')
-const cors = require("cors");
-const bodyParser = require('body-parser');
-const config = require("./config.json");
-const design = require("./functions/topdesign.js")
-const level = require("./functions/levels.js")
-const donation = require('./functions/donate.js');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const config = require('./config.json')
+const design = require('./functions/topdesign.js')
+const level = require('./functions/levels.js')
+const donation = require('./functions/donate.js')
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 app.use(cors({
   credentials: true,
   origin: true
-}));
-const port = process.env.PORT || 8080;
+}))
+const port = process.env.PORT || 8080
 
-const router = require('./routes.js');
+const router = require('./routes.js')
 app.get('/', function (req, res) {
-  res.send('Meddl!');
+  res.send('Meddl!')
 })
-app.get('/posts/month',design.findAllMonth); // Get all Posts sorted by Month
-app.get('/levels',level.findAll);
-app.post('/donate/', donation.add); // Add Donation
+app.get('/posts/month', design.findAllMonth) // Get all Posts sorted by Month
+app.get('/levels', level.findAll)
+app.post('/donate/', donation.add) // Add Donation
 
 // Getting API Tokens
 const tokens = config.apiKeys
 for (let i = 0; i < tokens.length; i++) {
-    app.use('/'+tokens[i], router);
+  app.use('/' + tokens[i], router)
 }
 
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.listen(port)
+console.log('Magic happens on port ' + port)
