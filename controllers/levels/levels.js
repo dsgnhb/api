853b3e426250c69c815fdcac9a8267d669e806b9
Flo <x.client.ich@gmail.com>
2017-10-31 21:43:13 +0100

    COINS

--- controllers/levels/levels.js
+++ controllers/levels/levels.js
@@ -1,9 +1,10 @@
 const con = require('../../helpers/Connection').getConnection()
 const xp = require('./xp')
 const chests = require('./chests')
+const coins = require('./coins')
 const Response = require('../../helpers/response-helper')
 
-  /**
+/**
    * @api {get} /levels Get all Level System data
    * @apiVersion 1.2.1
    * @apiName GetAllLevels
@@ -14,8 +15,8 @@ const Response = require('../../helpers/response-helper')
    *
    */
 
-exports.findAll = async function (req, res) {
-  con.query('SELECT userid, username, discriminator, avatar, xp, chests FROM discord_levels ORDER BY xp DESC', function (error, results) {
+exports.findAll = async function(req, res) {
+  con.query('SELECT userid, username, discriminator, avatar, xp, chests FROM discord_levels ORDER BY xp DESC', function(error, results) {
     if (error) throw error
     for (let i = 0; i < results.length; i++) {
       const element = results[i]
@@ -25,7 +26,7 @@ exports.findAll = async function (req, res) {
   })
 }
 
-  /**
+/**
    * @api {get} /levels/:userid Get Level System data by userid
    * @apiVersion 1.2.1
    * @apiName GetLevelsByUserId
@@ -37,16 +38,22 @@ exports.findAll = async function (req, res) {
    * @apiError not_found Not found (404)
    *
    */
-exports.findById = async function (req, res) {
+exports.findById = async function(req, res) {
   const userid = req.params.userid
-  con.query('SELECT username, discriminator, avatar, xp, chests, (SELECT COUNT(id) FROM discord_levels as dc_levels WHERE dc_levels.xp >= levels.xp) AS rank FROM discord_levels as levels WHERE userid = ?', [userid], function (error, results) {
-    if (error) throw error
-    results = results[0]
-    if (!results) return Response.not_found(res)
-    Response.success(res, results)
-  })
+  con.query(
+    'SELECT username, discriminator, avatar, xp, chests, (SELECT COUNT(id) FROM discord_levels as dc_levels WHERE dc_levels.xp >= levels.xp) AS rank FROM discord_levels as levels WHERE userid = ?',
+    [userid],
+    function(error, results) {
+      if (error) throw error
+      results = results[0]
+      if (!results) return Response.not_found(res)
+      Response.success(res, results)
+    }
+  )
 }
 exports.addXP = xp.addXP
 exports.deleteXP = xp.deleteXP
 exports.addChests = chests.addChests
 exports.deleteChests = chests.deleteChests
+exports.addCoins = coins.addCoins
+exports.deleteCoins = coins.deleteCoins
