2e2a28290841a8b1ce14f2b2aef8235d8771d1b7
Lukas <lukvonstrom@web.de>
2017-11-15 22:57:50 +0100

    Change to Singleton

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,7 +4,10 @@ let root_router = Router();
 
 root_router.get('/', getPackageVersion);
 
+let ver: number;
 function getPackageVersion(req, res) {
-    return res.json(JSON.parse(fs.readFileSync('./package.json', 'utf-8')).version);
+    if (!ver) {ver = JSON.parse(fs.readFileSync('./package.json', 'utf-8')).version; }
+
+    return res.json({version: ver});
 }
 export default root_router;
