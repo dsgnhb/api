8295b5b29dc5446434823647542faae6b339e296
Flo <x.client.ich@gmail.com>
2018-02-15 14:39:00 +0100

    removed pm2

--- src/app.ts
+++ src/app.ts
@@ -83,8 +83,9 @@ if (process.env.CI) {
     console.log('Config loaded:');
     console.log(C);
 }
-
+/*
 if (process.env.CI && process.env.CINOTEST) {
     process.exit(0);
 }
+*/
 export default app;
