5b64c7a5af4bd9515d3d5d900d06f3421b5ac2e1
Flo <x.client.ich@gmail.com>
2018-02-26 00:37:12 +0100

    Update donation.ts

--- src/models/validation/donation/donation.ts
+++ src/models/validation/donation/donation.ts
@@ -1,9 +1,9 @@
 import {Joi} from 'celebrate';
 
 const Schema = Joi.object().keys({
-    ip: Joi.string().alphanum().required(),
-    code: Joi.string(),
-    name: Joi.string()
+    ip: Joi.string().ip().required(),
+    code: Joi.string().required(),
+    name: Joi.string().required()
 });
 
 export default Schema;
