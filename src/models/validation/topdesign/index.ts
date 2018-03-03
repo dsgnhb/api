a6cb72d853f5ebe7784a11ff93ed3883e29dbb0a
Flo <x.client.ich@gmail.com>
2018-03-03 18:21:00 +0100

    :green_heart: Fixed CI Build

--- src/models/validation/topdesign/index.ts
+++ src/models/validation/topdesign/index.ts
@@ -2,7 +2,7 @@ import {Joi} from 'celebrate';
 
 module TopDesign {
     export const postIdSchema = Joi.object({
-        id: Joi.number().postive().required()
+        id: Joi.number().positive().required()
     });
 }
 
