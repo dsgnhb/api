39abe0a2595caab8fe8919fc91224642993ea4e7
Flo <x.client.ich@gmail.com>
2018-03-11 16:17:12 +0100

    :loud_sound: logs lol

--- src/controllers/updateimage/update.ts
+++ src/controllers/updateimage/update.ts
@@ -6,6 +6,7 @@ import {generateToken} from '../../util/util';
 export default async function(req, res) {
     try {
         let uuid = generateToken();
+        console.log(uuid);
         map.push({uuid: uuid, header1: req.body.header1, header2: req.body.header2, content: req.body.content, bgimg: req.body.bgimg});
         const browser = await puppeteer.launch({args: [
             '--hide-scrollbars',
