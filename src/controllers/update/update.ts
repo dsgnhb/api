95ed0f8eb552b14584ea487ff81ee82de90d260f
Flo <x.client.ich@gmail.com>
2018-03-11 20:06:38 +0100

    :construction: Deploying

--- src/controllers/update/update.ts
+++ src/controllers/update/update.ts
@@ -2,8 +2,8 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 module Update {
     export async function run_update(req, res) {
-        const {stdout, stderr} = await exec('../../update.sh');
-        res.json({stdout: stdout, stderr: stderr});
+        res.json({message: 'Updating!..'});
+        exec('../../../update.sh');
     }
 }
 export = Update;
